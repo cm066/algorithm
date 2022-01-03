76942b6754cd73ff1979544f5948e966af429117
cmcm <1446525549@qq.com>
2022-01-03 14:39:09 +0800

    回溯 46题全排列 修改方案

--- day30/src/main/java/back/Permute.java
+++ day30/src/main/java/back/Permute.java
@@ -2,7 +2,9 @@ package back;
 
 import org.jetbrains.annotations.Nls;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 /**
@@ -12,25 +14,64 @@ import java.util.List;
  */
 public class Permute {
     public static void main(String[] args) {
-
+        int[] nums = {0,1};
+        List<List<Integer>> permute = permute(nums);
+        System.out.println(permute);
     }
 
-    public List<List<Integer>> permute(int[] nums) {
+    /**
+     * 还有问题
+     * @param nums
+     * @return
+     */
+    public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> combine = new ArrayList<>();
+//        List<Integer> combine = new ArrayList<>();
         if (nums == null){
             return ans;
         }
-        helper(nums,0,ans,combine);
+        int len = nums.length;
+        Deque<Integer> path = new ArrayDeque<>();
+        boolean[] use = new boolean[len];
+        dfs(nums,len,0,ans,path,use);
+//        helper(nums,0,ans,combine);
         return ans;
     }
 
-    private void helper(int[] nums, int i, List<List<Integer>> ans, List<Integer> combine) {
+    /**
+     * 回溯就是根据不同的状态和回退上一步的操作来执行的
+     * @param nums
+     * @param len
+     * @param depth
+     * @param ans
+     * @param path
+     * @param use
+     */
+    private static void dfs(int[] nums, int len, int depth, List<List<Integer>> ans, Deque<Integer> path, boolean[] use) {
+        if (depth == len){
+            //说明到最后一层了，
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (use[i]){
+                continue;
+            }
+            path.add(nums[i]);
+            use[i] = true;
+            dfs(nums, len, depth+1, ans, path, use);
+            //回退到上一步，和之前坐相反到操作
+            path.removeLast();
+            use[i] = false;
+        }
+    }
+
+    private static void helper(int[] nums, int i, List<List<Integer>> ans, List<Integer> combine) {
         if (i == nums.length){
             ans.add(new ArrayList<>(combine));
             return;
         }
-        combine.add(nums[i-1]);
+        //combine.add(nums[i-1]);
         for (int j = 0; j < nums.length; j++) {
             combine.add(nums[i]);
             helper(nums,i+1,ans,combine);
