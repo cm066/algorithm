e1bd0ae9e3139a41a6bfe13482d4351dced8f713
cmcm <1446525549@qq.com>
2022-01-08 19:31:53 +0800

    208 中等 前缀树

---
+++ day30/src/main/java/tree/Trie.java
@@ -0,0 +1,53 @@
+package tree;
+
+import java.util.PriorityQueue;
+
+/**
+ * @author cm
+ * @create 2022/1/8-6:30 下午
+ * 208 中等 前缀树
+ */
+public class Trie {
+    private Trie[] children;
+    private boolean isEnd;
+    public Trie() {
+        children = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String word) {
+        Trie node = this;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null){
+                //这个是判断当前是否已经存在这字符来，没有就创建一个新添在这里
+                node.children[index] = new Trie();
+            }
+            node = node.children[index];
+        }
+        node.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        Trie node = searchPrefix(word);
+        return node != null && node.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        return searchPrefix(prefix) != null;
+    }
+
+    private Trie searchPrefix(String prefix) {
+        Trie node = this;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            int index = ch - 'a';
+            if (node.children[index] == null){
+                return null;
+            }
+            node = node.children[index];
+        }
+        return node;
+    }
+}
