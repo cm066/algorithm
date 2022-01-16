229ddba558df2e5556752c41d1666bd3dbf50b09
cmcm <1446525549@qq.com>
2022-01-16 22:14:48 +0800

    84. 柱状图中最大的矩形 困难

---
+++ day30/src/main/java/stack/LargestRectangleArea.java
@@ -0,0 +1,75 @@
+package stack;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+/**
+ * @author cm
+ * @create 2022/1/16-7:22 下午
+ * 84. 柱状图中最大的矩形 困难
+ */
+public class LargestRectangleArea {
+    public static void main(String[] args) {
+        int[] heights = {2,1,5,6,2,3};
+        int[] heights1 = {2,4};
+        int i = largestRectangleArea(heights);
+
+        System.out.println(i);
+    }
+
+    /**
+     * 采用单调栈的思想来解决
+     * @param heights
+     * @return
+     */
+    public static int largestRectangleArea(int[] heights) {
+        int len = heights.length;
+        if (len == 0){
+            return 0;
+        }
+        if (len == 1){
+            return heights[0];
+        }
+        int area = 0;
+        int[] newHeights = new int[len+2];
+        for (int i = 0; i < len; i++) {
+            newHeights[i+1] = heights[i];
+        }
+        len += 2;
+        heights = newHeights;
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.addLast(0);
+        for (int i = 1; i < len; i++) {
+            //peekLast() 返回栈顶元素但是不移除
+            while (heights[stack.peekLast()] > heights[i]){
+                int height = heights[stack.removeLast()];
+                int width = i - stack.peekLast()-1;
+                area =Math.max(area,width*height);
+            }
+            stack.addLast(i);
+        }
+        return area;
+    }
+
+    /**
+     * 暴力递归的方法
+     * @param heights
+     * @return
+     */
+    public static int largestRectangleArea1(int[] heights) {
+        int ret = 0;
+        for (int i = 0; i < heights.length; i++) {
+            int left = i;
+            int right = i;
+            while (left-1 >= 0 && heights[left-1] >= heights[i]){
+              left--;
+            }
+            while ( right +1 < heights.length && heights[right] >= heights[i]){
+               right++;
+            }
+            ret = Math.max(ret,(right+1-left)*heights[i]);
+        }
+        return ret;
+    }
+}
