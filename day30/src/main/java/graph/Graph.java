a8651dce3cdb3eb225e2b5d397931230deb34caf
Lufei <1446525549@qq.com>
2022-02-14 10:12:45 +0800

    新增拓扑排序，根据局部依赖顺序解决全部依赖的顺序

--- day30/src/main/java/graph/Graph.java
+++ day30/src/main/java/graph/Graph.java
@@ -97,6 +97,85 @@ public class Graph {
             }
         }
     }
+
+    /**
+     * 拓扑排序，根局部依赖顺序解决全部依赖的顺序
+     */
+    public void topoSortByKahn(){
+        // 统计每个顶点的入度
+        int[] inDegree = new int[v];
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                // i -> w
+                int w = adj[i].get(j);
+                inDegree[w]++;
+            }
+        }
+        LinkedList<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < v; i++) {
+            //这个表示没有其他依赖于它，可以先试着作为起点来解决
+            if (inDegree[i] == 0){
+                queue.add(i);
+            }
+        }
+        while (!queue.isEmpty()){
+            Integer i = queue.remove();
+            System.out.println("->"+i);
+            for (int j = 0; j < adj[i].size(); j++) {
+                Integer k = adj[i].get(j);
+                //这个可以理解为该点被依赖度减少1
+                inDegree[k]--;
+                //这里表示这个点的其它要依赖于它的已经没有了，然后又以它为其它，看他依赖其它什么点
+                if (inDegree[k] == 0){
+                    queue.add(k);
+                }
+            }
+        }
+
+    }
+
+    /**
+     * 利用dfs深度优先搜素来实现拓扑排序
+     */
+    public void topoSortByDFS() {
+        // 先构建逆邻接表，边 s->t 表示，s 依赖于 t，t 先于 s，这个相当于把依赖的关系给调转一下，这样需要额外的空间啊？
+        LinkedList<Integer> inverseAdj[] = new LinkedList[v];
+        for (int i = 0; i < v; i++) {
+            //初始化逆序需要用到的空间
+            inverseAdj[i] = new LinkedList<>();
+        }
+        //生成逆邻接表
+        for (int i = 0; i < v; i++) {
+            for (int j = 0; j < adj[i].size(); j++) {
+                //i -> w
+                int w = adj[i].get(j);
+                // w -> i
+                inverseAdj[w].add(i);
+            }
+        }
+        boolean[] visited = new boolean[v];
+        for (int i = 0; i < v; i++) {
+            if (visited[i] == false){
+                visited[i] = true;
+                dfs(i,inverseAdj,visited);
+            }
+        }
+
+    }
+
+    private void dfs(int vertex, LinkedList<Integer>[] inverseAdj, boolean[] visited) {
+        for (int i = 0; i < inverseAdj[vertex].size(); i++) {
+            int w = inverseAdj[vertex].get(i);
+            if (visited[w] == true) {
+                continue;
+            }
+            visited[w] = true;
+            dfs(w,inverseAdj,visited);
+        }
+        // 先把 vertex 这个顶点可达的所有顶点都打印出来之后，再打印它自己
+        System.out.println("->"+vertex);
+    }
+
     public static void main(String[] args) {
         Graph graph = new Graph(8);
         graph.addEdge(0,1);
