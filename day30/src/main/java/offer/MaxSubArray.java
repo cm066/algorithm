014b394a95b4c688a8030b23956c55aa720345c1
cmcm <1446525549@qq.com>
2022-03-30 20:13:22 +0800

    剑指 Offer 42. 连续子数组的最大和

---
+++ day30/src/main/java/offer/MaxSubArray.java
@@ -0,0 +1,40 @@
+package offer;
+
+/**
+ * @author cm
+ * @create 2022/3/30-7:35 下午
+ * 剑指 Offer 42. 连续子数组的最大和
+ * 输入一个整型数组，数组中的一个或连续多个整数组成一个子数组。求所有子数组的和的最大值。
+ *
+ * 要求时间复杂度为O(n)。
+ *
+ *
+ *
+ * 示例1:
+ *
+ * 输入: nums = [-2,1,-3,4,-1,2,1,-5,4]
+ * 输出: 6
+ * 解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+ */
+public class MaxSubArray {
+    public static void main(String[] args) {
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+        int i = maxSubArray(nums);
+        System.out.println(i);
+    }
+
+    /**
+     * 优化空间
+     * @param nums
+     * @return
+     */
+    public static int maxSubArray(int[] nums) {
+        int pre = 0, maxAns = nums[0];
+        for (int x : nums) {
+            pre = Math.max(pre + x, x);
+            maxAns = Math.max(maxAns, pre);
+        }
+        return maxAns;
+    }
+
+}
